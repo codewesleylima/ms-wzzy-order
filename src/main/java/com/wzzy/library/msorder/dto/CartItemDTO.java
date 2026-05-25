0c2fb7df93cabaf30f397cb9f631d39fe7f9f5d2
codewesleylima <gusterlima@hotmail.com>
2026-05-24 23:45:22 -0300

    feat(ms-order): add CartItemDTO

---
+++ src/main/java/com/wzzy/library/msorder/dto/CartItemDTO.java
@@ -0,0 +1,25 @@
+package com.wzzy.library.msorder.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemDTO {
+    private String id;
+    private String shoppingCartId;
+    private String bookId;
+    private String bookTitle;
+    private int quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+    private String isbn;
+    private String addedAt;
+    private String updatedAt;
+}
