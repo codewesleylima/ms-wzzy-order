50fa0b7f8b0a74ac03993a5abe266c45f9b21e6c
codewesleylima <gusterlima@hotmail.com>
2026-05-25 22:51:15 -0300

    chore: Remove all Java classes - keep folder structure and infrastructure

--- src/main/java/com/wzzy/library/msorder/exception/CartEmptyException.java
+++
@@ -1,9 +0,0 @@
-package com.wzzy.library.msorder.exception;
-
-import com.wzzy.library.bfflibrary.exception.ValidationException;
-
-public class CartEmptyException extends ValidationException {
-    public CartEmptyException() {
-        super("Shopping cart is empty. Add items before checkout.", "CART_EMPTY");
-    }
-}
--- src/main/java/com/wzzy/library/msorder/exception/CheckoutFailedException.java
+++
@@ -1,9 +0,0 @@
-package com.wzzy.library.msorder.exception;
-
-import com.wzzy.library.bfflibrary.exception.BaseServiceException;
-
-public class CheckoutFailedException extends BaseServiceException {
-    public CheckoutFailedException(String reason) {
-        super("Checkout failed: " + reason, "CHECKOUT_FAILED", 400);
-    }
-}
